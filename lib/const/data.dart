b9a492c638fbca2ab84abeb8e70b49e428571be7
imminsung <htk4064@flexenm.com>
2023-10-22 20:47:41 +0900

    status_model 작성

---
+++ lib/const/data.dart
@@ -0,0 +1,2 @@
+const serviceKey =
+    'AX5dHj13+RdwIVoRV9Hte0eGPDs7zvHdvqbUiNBOPjUf7CT/uABVKYCTPiEiJcRgAR1rGX67xW1WmGPXeD4bcg==';
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -4,9 +4,27 @@ import 'package:flutter_dusty_dust/component/hourly_cart.dart';
 import 'package:flutter_dusty_dust/component/main_app_bar.dart';
 import 'package:flutter_dusty_dust/component/main_drawer.dart';
 import '../const/colors.dart';
+import '../repository/stat_repository.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    final statModels = await StatRepository.fetchData();
+
+    print(statModels);
+  }
 
   @override
   Widget build(BuildContext context) {
